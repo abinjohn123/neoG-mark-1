8e32f516091949edfcc066c7c8c092c3b095d4ed
Abin John <abinjohnanto@gmail.com>
2022-09-06 18:18:19 +0530

    create questions

---
+++ script.js
@@ -0,0 +1,26 @@
+const R = require('readline-sync');
+
+const questions = [
+  {
+    question: 'Which city is Abin based off of?: ',
+    answer: 'thrissur',
+  },
+  {
+    question: 'Is he a coffee or tea person?: ',
+    answer: 'coffee',
+  },
+  {
+    question:
+      'What fitness activity is he into these days? (Running, Cycling, Weights): ',
+    answer: 'cycling',
+  },
+  {
+    question:
+      'What is his favourite programing language? (C, Java, JavaScript): ',
+    answer: 'javascript',
+  },
+  {
+    question: 'Which brand mobile does he use? (Moto, Realme, Redmi): ',
+    answer: 'realme',
+  },
+];
